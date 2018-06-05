60f187e2084882a1edac35c41c1c596a9aff2c92
Kaiser <kaiser@Yathirajans-MacBook-Air.local>
2018-06-05 21:47:16 +0530

    added Choice and Option js files

--- src/Choice.js
+++ src/Choice.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
 const Choice = function(props){
+    
+
     return (
         <label>
-        <input type = 'radio' value = {props.id}/>
+        <input type = 'radio' value = {props.id}
+        checked = {props.checked}
+        onChange = {props.onChange}
+        />
         {props.value}
         </label>
     )
--- src/Option.js
+++ src/Option.js
@@ -2,21 +2,40 @@ import React from 'react'
 import Choice from './Choice'
 
 const Choices = [
-    {id: 1, value: "Option 1"},
-    {id: 2, value: "Option 2"}, 
-    {id: 3, value: "Option 3"},
-    {id: 4, value: "Option 4"}
+    {id: "1", value: "Option 1"},
+    {id: "2", value: "Option 2"}, 
+    {id: "3", value: "Option 3"},
+    {id: "4", value: "Option 4"}
 ]
 
 
 
 class Option extends React.Component{
+    constructor(){
+        super()
+
+        this.state = {
+            selected: null
+        }
+    }
+
+    onSelect = (e)=>{
+        debugger;
+        this.setState({
+            selected : e.target.value
+        })
+    }
+
     render(){
         return(
             <div>
                 {Choices.map((choice, i)=>{
                     return(
-                        <Choice id = {choice.id} value = {choice.value}/>
+                        <Choice id = {choice.id} 
+                        value = {choice.value}
+                        checked = {this.state.selected === choice.id}
+                        onChange = {this.onSelect}
+                        key = {i}/>
                     )
                 })}
                 </div>
